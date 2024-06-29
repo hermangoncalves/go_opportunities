3bd3bc84098c39db049462fdbac561643a81a5f5
hermangoncalves <hermangoncalves@outlook.com>
2024-06-29 01:19:19 -0100

    feat:add swagger configuration

--- handler/response.go
+++ handler/response.go
@@ -5,21 +5,48 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hermangoncalves/go_opportunities/schemas"
 )
 
 func sendSuccess(ctx *gin.Context, op string, data interface{}) {
 	ctx.Header("Content-Type", "application/json")
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("Operation from handler: %s successfull", op),
-		"data": data,
+		"data":    data,
 	})
 }
-
 
 func sendError(ctx *gin.Context, code int, message string) {
 	ctx.Header("Content-Type", "application/json")
 	ctx.JSON(code, gin.H{
-		"message": message,
+		"message":   message,
 		"errorCode": code,
 	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode string `json:"errorCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ListOpeningsResponse struct {
+	Message string                    `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
--- router/routes.go
+++ router/routes.go
@@ -3,15 +3,19 @@ package router
 import (
 	"fmt"
 	"net/http"
-
+	"github.com/hermangoncalves/go_opportunities/docs"
 	"github.com/gin-gonic/gin"
 	"github.com/hermangoncalves/go_opportunities/handler"
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+	swaggerfiles "github.com/swaggo/files" // swagger embed files
+
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize handler
 	handler.InitializeHandler()
 	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	v1 := router.Group(basePath)
 	{
 		v1.GET("", func (ctx *gin.Context)  {
@@ -30,4 +34,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.GET("/openings", handler.ListOpeningHandler)
 
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
